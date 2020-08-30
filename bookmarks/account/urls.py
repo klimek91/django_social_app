a135e013af19d676cf847c442ad94d1f58e012a5
Klimek <klimkowskib@gmail.com>
2020-08-30 16:20:06 +0200

    create 4 urls for password reset, also reset form and reset email templates

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -10,4 +10,9 @@ urlpatterns = [
     # dwa ponizsze adresy url sa przeznaczone do obslugi zmiany hasla
     path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    # czterny ponizsze adresy url sa przeznazone do obslugi zerwoania hasla
+    path('password_reset/',auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/',auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
