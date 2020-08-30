cda4deb2cadc894ded6ddf089caabf04835fb01d
Klimek <klimkowskib@gmail.com>
2020-08-30 12:39:37 +0200

    create login/logout urls and new templates

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    #widok logowania
-    path('login/', views.user_login, name='login'),
+    #path('login/', views.user_login, name='login'),    poprzedni  widok login view
+    path('login/', auth_views.LoginView.as_view(), name = 'login'),
+    path('logout/', auth_views.LogoutView.as_view(), name= 'logout'),
 ]
