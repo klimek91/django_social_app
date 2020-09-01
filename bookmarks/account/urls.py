bc319e07d0dcd6a219111ba9eaa47f3c9d2bf008
Klimek <klimkowskib@gmail.com>
2020-09-01 18:55:51 +0200

    password reset done

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 from django.contrib.auth import views as auth_views
 
@@ -15,4 +15,5 @@ urlpatterns = [
     path('password_reset/done/',auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('', include('django.contrib.auth.urls')),
 ]
