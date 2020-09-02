e2a360d9b77c293228cda182eff54cfcebec7261
Klimek <klimkowskib@gmail.com>
2020-09-02 22:33:30 +0200

    add edit view, url, template. add pass change and edit profil to dashboard template

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm, UserRegistrationForm
+from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from django.contrib.auth.decorators import login_required
-
+from .models import Profile
 
 # Create your views here.
 
@@ -36,7 +36,22 @@ def register(request):
             new_user = user_form.save(commit=False) #utworzenie nowego obiektu uzytkownika ale jezscze NIE zapisanie go w bazie danych
             new_user.set_password(user_form.cleaned_data['password']) #ustawienie wybranego hasła
             new_user.save() #zapisanie obiektu user w bazie danych
+            # utowrzenie profilu użytkownika
+            profile = Profile.objects.create(user=new_user)
             return render(request, 'account/register_done.html', {'new_user':new_user})
     else:
         user_form = UserRegistrationForm()
     return render(request, 'account/register.html', {'user_form':user_form})
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = ProfileEditForm(instance=request.user.profile, data=request.POST, files=request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+    return render(request, 'account/edit.html/', {'user_form':user_form, 'profile_form':profile_form})
